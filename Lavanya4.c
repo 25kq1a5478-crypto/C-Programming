a0189c54d3555aff56dfecfa01be30ea285c0183
25kq1a5478-crypto <25kq1a5478@pace.ac.in>
2025-11-21 20:16:29 +0530

    Add files via upload

---
+++ Lavanya4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b;
+  printf("enter two numbers");
+  scanf("%d%d",&a,&b);
+  if(a>b)
+  {
+  printf("%d is the biggest number\n",a);
+  }
+  else
+  {
+  printf("%d is the biggest number\n",b);
+  }
+  getch();
+  }
+  
