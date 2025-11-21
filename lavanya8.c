fe8c5a354b35a997e259e6ded34189ae0bf5d523
25kq1a5478-crypto <25kq1a5478@pace.ac.in>
2025-11-21 20:41:49 +0530

    Add files via upload

---
+++ lavanya8.c
@@ -0,0 +1,19 @@
+//write a program given number is positive or negative 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num;
+  printf("enter a number");
+  scanf("%d",&num);
+  if(num>0)
+  {
+  printf("The number is positive\n");
+  }
+  else
+  {
+  printf("The number is negative\n");
+  }
+  getch();
+  }
+  
