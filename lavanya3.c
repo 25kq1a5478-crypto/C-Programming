6353ac998571976fdbc40f41ffd8d5308be0fd67
25kq1a5478-crypto <25kq1a5478@pace.ac.in>
2025-11-21 19:34:28 +0530

    Add files via upload

---
+++ lavanya3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char ch;
+  clrscr();
+  printf("enter char:");
+  scanf("%c",ch);
+  switch(ch)
+  {
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'u':
+  printf("Given char is vowel");
+  break;
+  default:printf("Given char is consonant");
+  }
+    getch();
+}
