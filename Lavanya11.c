a7b1b9e4cb90b8420f5ae826b5caeea68ea3f71b
25kq1a5478-crypto <25kq1a5478@pace.ac.in>
2025-11-23 17:19:18 +0530

    Add files via upload

---
+++ Lavanya11.c
@@ -0,0 +1,16 @@
+//write a program to print numbers from 10 to 1 reverse sequencial orderencial 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i;
+  clrscr();
+  i=10;
+  while(i>=2)
+  {
+  printf("%d ",i);
+  i=i-1;
+  }
+  getch();
+  }
+  
---
+++ Lavanya12.c
@@ -0,0 +1,16 @@
+//write a program to print 1-10 numbers using while
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i;
+  clrscr();
+  i=1;
+  while(i<=5)
+  {
+  printf("%d ",i);
+  i=i+1;
+  }
+  getch();
+  }
+  
